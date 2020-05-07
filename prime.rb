216febbdb616eec23a25229372934ea678842d85
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-07 01:49:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(int)
+  if int >= 2
+    (2..int - 1).none? { |num| int % num == 0}
+  elsif int <= -2 
+    (int + 1..-2).none? { |num| int % num == 0}
+  else
+    false
+  end
+end
